28ab8ed47c2df75894ee262e9edbf8058fcceb8c
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-07-03 11:10:22 -0300

    getting assets and initializing hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image"
+import bgStars from "./assets/bg-stars-1.webp"
+import heroLine from "./assets/hero-line-2.svg"
+
 export default function Home() {
   return (
     <main>
-    New page
+      <section className="h-screen relative w-screen bg-hero bg-cover bg-no-repeat">
+        <div className="max-w-[1250px] mx-auto relative">
+        <Image
+          src={bgStars}
+          width={500}
+          height={500}
+          alt="background_stars/webp"
+          className="w-[500px] h-[500px] absolute right-16 top-20"
+        />
+          <div className="absolute left-0 top-20">
+            <div className="bg-[#6639BA] absolute w-[4px] h-[500px]"/>
+            <div className="relative">
+              <div className="bg-[#6639BA] absolute -right-[2px] w-3 h-3 rounded-full border" />
+              <Image
+                src={heroLine}
+                width={179}
+                height={128}
+                alt="hero_line/svg"
+                className="w-[179px] h-[128px]"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
