99d5496a70bd1c40b920647da4728d4e7823aeb4
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-07-13 12:54:50 -0300

    branch security component refactoring done

--- src/app/components/sections/security/integrate/Branch.tsx
+++ src/app/components/sections/security/integrate/Branch.tsx
@@ -1,21 +1,18 @@
 import bgStars from "@/app/assets/bg-stars-1.webp"
 import branchSecurity from "@/app/assets/git-branch-security.svg"
 import { TextSpanWithUnderline } from "@/app/components/globals/TextSpanWithUnderline"
+import { branchStyles as css } from "./styles"
 
 export const Branch = () => {
   return (
-    <div className="flex relative pb-9">
-      <img
-        src={bgStars.src}
-        alt="bg_stars/webp"
-        className="w-[500px] rotate-[380deg] md:w-[1000px] h-[900px] z-0 -bottom-[280px] absolute right-0"
-      />
-      <div className="min-h-full -mt-[38px] w-[81px] relative">
-        <div className="absolute left-[17px] md:left-[53px] top-0 w-[2.5px] md:w-[4px] h-[800px] bg-gradient-to-b z-20 from-[#2b2c68] via-[#5c61bc] to-[#2b2c68]" />
+    <div className={css.wrapper}>
+      <img src={bgStars.src} alt="bg_stars/webp" className={css.bgStars} />
+      <div className={css.securityBranchContainer}>
+        <div className={css.securityBranchLine} />
       </div>
-      <div className="py-[5px] -ml-[35px] md:ml-0 md:pl-[64px] pr-[8px] md:pr-[24px] mt-[180px] md:mt-[245px] lg:my-[64px]">
-        <p className="text-[20px] ssm:text-[24px] text-[#7d8590] font-inter leading-[28px] md:leading-[32px] max-w-[317px] smm:max-w-[500px] w-full md:max-w-[500px] font-medium mb-[24px] md:mb-[16px]">
-          <span className="text-[#f0f0f0]">GitHub Advanced Security </span>
+      <div className={css.contentContainer}>
+        <p className={css.paragraph}>
+          <span className={css.spanStrong}>GitHub Advanced Security </span>
           lets you gain visibility into your security posture, respond to
           threats proactively, and ship secure applications quickly.
         </p>
@@ -23,24 +20,18 @@ export const Branch = () => {
           title="Get GitHub Advanced Security"
           styles="text-[#f0f0f0] mt-4"
         />
-        <div className="mt-[64px] pt-[48px] pb-[100px]">
-          <div className="font-roboto relative">
-            <img
-              src={branchSecurity.src}
-              alt="git_branch_security/svg"
-              className="absolute hidden md:block -left-[93px] -top-[180px]"
-            />
-            <span className="text-[#939aff] font-inter block text-[12px] leading-[20px] font-semibold rounded-full border border-[#939aff] w-fit px-[8px]">
-              Did you know?
-            </span>
-            <h3 className="bg-gradient-to-br from-[#939aff] to-[#5c61bc] text-transparent bg-clip-text font-jet-brains my-[8px] text-[32px] md:text-[64px] leading-[36px] md:leading-[68px] tracking-[-5px]">
-              56 million projects
-            </h3>
-            <span className="text-base md:text-[20px] leading-[24px] md:leading-[28px] font-medium flex">
-              fixed vulnerabilities with GitHub
-              <span className="text-base -mt-1 block">3</span>
-            </span>
-          </div>
+        <div className={css.infoContainer}>
+          <img
+            src={branchSecurity.src}
+            alt="git_branch_security/svg"
+            className={css.branchSecurity}
+          />
+          <span className={css.didYouKnow}>Did you know?</span>
+          <h3 className={css.didYouKnowData}>56 million projects</h3>
+          <span className={css.source}>
+            fixed vulnerabilities with GitHub
+            <span className={css.note}>3</span>
+          </span>
         </div>
       </div>
     </div>
--- src/app/components/sections/security/integrate/styles.ts
+++ src/app/components/sections/security/integrate/styles.ts
@@ -12,5 +12,17 @@ export const headerStyles = {
 }
 
 export const branchStyles = {
-  
+  wrapper: `flex relative pb-9`,
+  bgStars: `w-[500px] rotate-[380deg] md:w-[1000px] h-[900px] z-0 -bottom-[280px] absolute right-0`,
+  securityBranchContainer: `min-h-full -mt-[38px] w-[81px] relative`,
+  securityBranchLine: `absolute left-[17px] md:left-[53px] top-0 w-[2.5px] md:w-[4px] h-[800px] bg-gradient-to-b z-20 from-[#2b2c68] via-[#5c61bc] to-[#2b2c68]`,
+  contentContainer: `py-[5px] -ml-[35px] md:ml-0 md:pl-[64px] pr-[8px] md:pr-[24px] mt-[180px] md:mt-[245px] lg:my-[64px]`,
+  paragraph: `text-[20px] ssm:text-[24px] text-[#7d8590] font-inter leading-[28px] md:leading-[32px] max-w-[317px] smm:max-w-[500px] w-full md:max-w-[500px] font-medium mb-[24px] md:mb-[16px]`,
+  spanStrong: `text-[#f0f0f0]`,
+  infoContainer: `mt-[64px] pt-[48px] pb-[100px] font-roboto relative`,
+  branchSecurity: `absolute hidden md:block -left-[93px] -top-[180px]`,
+  didYouKnow: `text-[#939aff] font-inter block text-[12px] leading-[20px] font-semibold rounded-full border border-[#939aff] w-fit px-[8px]`,
+  didYouKnowData: `bg-gradient-to-br from-[#939aff] to-[#5c61bc] text-transparent bg-clip-text font-jet-brains my-[8px] text-[32px] md:text-[64px] leading-[36px] md:leading-[68px] tracking-[-5px]`,
+  source: `text-base md:text-[20px] leading-[24px] md:leading-[28px] font-medium flex`,
+  note: `text-base -mt-1 block`,
 }
